07cefbe3b3a3d9bbf4c9fb7eab10024fcd863e27
davydka <davydka@gmail.com>
2018-09-14 16:13:24 -0400

    adding create and update mutations

--- schema.js
+++ schema.js
@@ -19,59 +19,184 @@ const {
   GraphQLNonNull
 } = graphql;
 
+const {
+  Schema
+} = mongoose;
+
+
+
+
+/* MONGO */
 mongoose.connect(`mongodb://${mUSER}:${mPASS}@${mHOST}:${mPORT}/${mDB}?authSource=admin`, {
   useNewUrlParser: true
 });
 
-// mongo schema
-const TODO = mongoose.model('Todo', {
-  id: mongoose.Schema.Types.ObjectId,
-  title: String,
-  completed: Boolean
-})
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to mongodb');
+});
 
-  /*
-var newTodo = new TODO({
-  title: args.title,
-  completed: false
-})
-newTodo.id = newTodo._id
+const noteSchema = new Schema({
+  // id: Schema.Types.ObjectId,
+  id: String,
+  title: String,
+  content: String
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+// select * from notes
+Note.find(function (err, notes) {
+  if (err) return console.error(err);
+  console.log(notes);
+});
+
+/*
+Note.findOne({ _id: "5b9bcaf3999b449167925374" }, function (err, note) {
+  if (err) return console.error(err);
+  console.log(note);
+});
 */
 
-  /*
-var TodoType = new GraphQLObjectType({  
-  name: 'Todo',
+/*
+const mainNote = new Note({
+  title: 'Main Note',
+  content: 'lorem ipsum dolor'
+});
+console.log(mainNote);
+// mongoose.save?
+*/
+
+/* END MONGO */
+
+
+
+
+/* GraphQL */
+const NotesType = new GraphQLObjectType({
+  name: 'User',
   fields: () => ({
-    todos: {
-      type: new GraphQLList(TodoType),
-      resolve: () => {
-        return new Promise((resolve, reject) => {
-          TODO.find((err, todos) => {
-            if (err) reject(err)
-            else resolve(todos)
-          })
-        })
-      }
-    }
+    id: { type: GraphQLString },
+    title: { type: GraphQLString },
+    content: { type: GraphQLString },
   })
 });
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    user: {
-      type: UserType,
+    note: {
+      type: NotesType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return axios.get(`http://localhost:3000/users/${args.id}`)
-          .then(resp => resp.data);
+        return new Promise((resolve, reject) => {
+          Note.findOne({ _id: args.id }, function (err, note) {
+            if (err) { 
+              console.error(err);
+              resolve(err);
+              return;
+            }
+            else {
+              console.log(note);
+              resolve(note);
+            }
+          });
+        });
       }
     }
   }
 });
 
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addNote: {
+      type: NotesType,
+      args: {
+        title: { type: GraphQLString },
+        content: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parentValue, { title, content }) {
+        return new Promise((resolve, reject) => {
+          const newNote = new Note({
+            title: title,
+            content: content
+          });
+
+          newNote.id = newNote._id;
+          // resolve(newNote);
+          newNote.save(function (err, note) {
+            if (err) { 
+              console.error(err);
+              resolve(err);
+              return;
+            }
+            else {
+              console.log(note);
+              resolve(note);
+            }
+          });
+        });
+      }
+    },
+    editNote: {
+      type: NotesType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        title: { type: GraphQLString },
+        content: { type: GraphQLString },
+      },
+      resolve(parentValue, args) {
+        return new Promise((resolve, reject) => {
+          Note.findOne({ _id: args.id }, function (err, note) {
+            if (err) {
+              console.error(err);
+              resolve(err);
+              return;
+            }
+            
+            note.title = args.title;
+            note.content = args.content;
+            note.save(function (err, updatedNote) {
+              if (err) { 
+                console.error(err);
+                resolve(err);
+                return;
+              }
+              else {
+                console.log(updatedNote);
+                resolve(updatedNote);
+              }
+            });
+          });
+        });
+      }
+    },
+    /*
+    deleteNote: {
+      type: NotesType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parentValue, { id }) {
+      }
+    }
+    */
+  }
+
+});
+/* END GraphQL */
+
+
+
+
+
+
+
+
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
-  // mutation
+  mutation
 });
-*/
